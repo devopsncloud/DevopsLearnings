0d537e802bbb7a16a09bc39bafc94223e445a711
srikanth <srikanth.sama678@gmail.com>
2020-09-08 13:29:50 -0700

    printing commit

--- shellscripting/printing.sh
+++ shellscripting/printing.sh
@@ -9,3 +9,24 @@ echo How are you!!!
 
 echo -e "Hello  srikanth\n Good Morning!!!"
 
+# color codes
+#---------------------------
+#color         code
+#---------------------------
+#Red   --------->   31
+#Green --------->   32
+#Yellow --------->  33
+#Blue   --------->  34
+#Magneta ---------> 35
+#Cyan   --------->  36
+
+
+#syantax : echo -e "\e[COLOR-CODEmMESSAGE"
+
+echo -e "\e[30mSRIAKNTH"
+echo -e "\e[31mSRIAKNTH"
+echo -e "\e[32mSRIAKNTH"
+echo -e "\e[33mSRIAKNTH"
+echo -e "\e[34mSRIAKNTH"
+echo -e "\e[35mSRIAKNTH"
+echo -e "\e[36mSRIAKNTH"
