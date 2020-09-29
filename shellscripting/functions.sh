eb68e893527d9ac37e50772c1419998da9e98873
srikanth <srikanth.sama678@gmail.com>
2020-09-29 12:09:04 -0700

    functions commit

--- shellscripting/functions.sh
+++ shellscripting/functions.sh
@@ -5,11 +5,15 @@ simplefunction()    #function declartion
 
 echo I am srikanth
 echo $a
-
+echo $b
 }
 
 #Calling a function
+
+#Main program
  a=10
  simplefunction
+
+ b='srikanth kashapaka'
 
   
