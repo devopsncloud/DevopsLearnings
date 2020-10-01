5ddb4a9e4a496eab8479f98bc913efc60317d674
srikanth <srikanth.sama678@gmail.com>
2020-10-01 15:52:00 -0700

    project commit

--- shellscripting/project.sh
+++ shellscripting/project.sh
@@ -12,9 +12,16 @@
 #User
 #All
 
+#Creating a temporary log file to hide terminal output
+
+LOG_FILE = /tmp/roboshop.log  #creates a temp logfile
+rm -f $LOG_FILE               # Clears the log content before the script runs
+
+
+
 Frontend() {
     echo "Installing Frontend Service"
-    yum install nginx -y
+    yum install nginx -y &>$LOG_FILE     #output of the installation is redirecting to logfile
     case $? in
          0)
         echo "Nginx Install - SUCCESS"
