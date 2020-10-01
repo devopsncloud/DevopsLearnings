493f8c5e4b25f70e0c38ca3d19984a55980716b5
srikanth <srikanth.sama678@gmail.com>
2020-10-01 16:23:23 -0700

    project commit

--- shellscripting/project.sh
+++ shellscripting/project.sh
@@ -21,7 +21,7 @@ rm -f $LOG_FILE               # Clears the log content before the script runs
 
 Frontend() {
     echo "Installing Frontend Service"
-    yum install nginx -y &>$LOG_FILE     #output of the installation is redirecting to logfile
+    yum install ngiinx -y &>$LOG_FILE     #output of the installation is redirecting to logfile
     case $? in
          0)
         echo "Nginx Install - SUCCESS"
