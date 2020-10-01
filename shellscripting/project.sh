fc46cedd30f4df3ebbbbd6710195e4b76f94c524
srikanth <srikanth.sama678@gmail.com>
2020-10-01 13:44:15 -0700

    project commit

--- shellscripting/project.sh
+++ shellscripting/project.sh
@@ -57,8 +57,8 @@ User() {
 } 
 
 USAGE() {
-    echo -e "Usage \t\t : $0 Component"
-    echo -e "Components \t : Frontend, MongoDB, Redis, MySQL, RabbitMQ , Cart, Catalogue, Shipping, Payment, User"
+    echo -e "Usage \t\t   : $0 Component"
+    echo -e "Components \t   : Frontend, MongoDB, Redis, MySQL, RabbitMQ , Cart, Catalogue, Shipping, Payment, User"
     echo For all Components : All
     exit 1
 }
