04d2f39c7684b6fc25b0d6b22ec25fb83fabfae9
srikanth <srikanth.sama678@gmail.com>
2020-10-02 22:57:51 -0700

    project commit

--- shellscripting/project.sh
+++ shellscripting/project.sh
@@ -34,7 +34,7 @@ case $1 in
 Frontend() {
     Heading "Installing Frontend Service"
     yum install nginx -y &>>$LOG_FILE     # terminal output redirecting to log file
-    Stat $? "Nginx-Install"
+    Stat $? "Nginx-Install\t\t"
     curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/98e5c57f-66c8-4828-acd6-66158ed6ee33/_apis/git/repositories/65042ce1-fdc2-4472-9aa2-3ae9b87c1ee4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
     Stat $? "Download frontend files" &>>$LOG_FILE
 
@@ -50,7 +50,7 @@ Frontend() {
 
     systemctl enable nginx &>>$LOG_FILE
     systemctl start nginx &>>$LOG_FILE
-    Stat $? "Start Nginx"
+    Stat $? "Start Nginx\t\t"
 }
 
 
