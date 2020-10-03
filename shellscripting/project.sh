ce63841c39da6db29bd270f8bbb0b017395816b3
srikanth <srikanth.sama678@gmail.com>
2020-10-03 10:55:11 -0700

    project commit

--- shellscripting/project.sh
+++ shellscripting/project.sh
@@ -56,6 +56,35 @@ Frontend() {
 
 MongoDB() {
     Heading "Installing MongoDB Service"
+    echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
+yum install -y mongodb-org &>>$LOG_FILE
+Stat $? "Install MongoDB Service"
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongodb &>>$LOG_FILE
+Stat $? "Start Mongodb Service"
+
+cd/tmp
+curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/
+98e5c57f-66c8-4828-acd6-66158ed6ee33/_apis/git/repositories/52feee4a-7c54-4f95-b1f5-2051a56b9d76/
+items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&
+versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>>LOG_FILE
+
+Stat $? "Download Mongodb Schema"
+
+unzip /tmp/mongodb.zip &>>$LOG_FILE
+Stat $? "Extract Mongodb Schema"
+
+mongo < catalogue.js &>>$LOG_FILE
+Stat $? "Load Catalogue Schema"
+
+mongo < users.js &>>$LOG_FILE
+Stat $? "Load User Schema"
 }
 
 
