c18ab660421e83e49a49e89216fe91a6e076767a
srikanth <srikanth.sama678@gmail.com>
2020-10-03 17:29:46 -0700

    project commit

--- shellscripting/project.sh
+++ shellscripting/project.sh
@@ -87,15 +87,12 @@ Stat $? "Load User Schema\t"
 
 
 MySQL() { 
-    yum list installed | grep mysql-community-server  &>/dev/null
+    yum list installed | grep mysql-community-server &>/dev/null
     case $? in 
        0) 
-       true
-       ;;
-
+         true
+         ;;
        *) 
-
-
     Heading "Installing MySQL Service"
     curl -L -o /tmp/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar &>>$LOG_FILE
     Stat $? "Download MySQL Bundle \t"
@@ -105,11 +102,12 @@ MySQL() {
     Stat $? "Extract MySQL Bundle \t"
 
     yum remove mariadb-libs -y &>>$LOG_FILE
-    yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm mysql-community-common-5.7.28-1.el7.x86_64.rpm mysql-community-libs-5.7.28-1.el7.x86_64.rpm mysql-community-server-5.7.28-1.el7.x86_64.rpm -y 
+    yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm mysql-community-common-5.7.28-1.el7.x86_64.rpm mysql-community-libs-5.7.28-1.el7.x86_64.rpm mysql-community-server-5.7.28-1.el7.x86_64.rpm -y &>>$LOG_FILE
     
     Stat $? "Install MySQL Database\t"
     ;;
 esac
+
     systemctl enable mysqld &>>$LOG_FILE
     systemctl start mysqld &>>$LOG_FILE
     Stat $? "Start MySQL Database Server"
