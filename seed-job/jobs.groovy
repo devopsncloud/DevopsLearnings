2c397c8b4effb45f7724834eaefbaa40de16b684
learn2earnSRK <68834522+learn2earnSRK@users.noreply.github.com>
2020-12-12 20:52:13 -0800

    Update jobs.groovy

--- seed-job/jobs.groovy
+++ seed-job/jobs.groovy
@@ -1,1 +1,22 @@
-
+pipelineJob("Cart-CI") {
+  description('CI-JOB Cart-Service')
+  definintion{
+    cpsScm{
+      scm{
+        git{
+          remote{
+            url("https://github.com/learn2earnSRK/DevopsLearnings.git")
+                 }
+          branch("*/master")
+        }
+      }
+      scriptpath("Jenkinsfile")
+    }
+  }
+}
+      
+                 
+          
+          
+            
+    
